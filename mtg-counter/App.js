0c508d775babbe37fc90a954b724701970207796
Nick <williamsne2@gmail.com>
2019-07-01 18:10:57 -0500

    renders player 1 upside down, flex flow is horizontal

--- mtg-counter/App.js
+++ mtg-counter/App.js
@@ -3,11 +3,50 @@ import { StyleSheet, Text, View } from "react-native";
 import LifeBox from "./components/LifeBox";
 
 export default class App extends React.Component {
+  state = {
+    p1LifeTotal: 20,
+    p2LifeTotal: 20
+  };
+
+  incrementP1 = () => {
+    let p1LifeTotal = this.state.p1LifeTotal;
+    p1LifeTotal++;
+    this.setState({ p1LifeTotal });
+  };
+
+  incrementP2 = () => {
+    let p2LifeTotal = this.state.p2LifeTotal;
+    p2LifeTotal++;
+    this.setState({ p2LifeTotal });
+  };
+
+  decrementP1 = () => {
+    let p1LifeTotal = this.state.p1LifeTotal;
+    p1LifeTotal--;
+    this.setState({ p1LifeTotal });
+  };
+
+  decrementP2 = () => {
+    let p2LifeTotal = this.state.p2LifeTotal;
+    p2LifeTotal--;
+    this.setState({ p2LifeTotal });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <LifeBox />
-        <LifeBox />
+        <LifeBox
+          lifetotal={this.state.p1LifeTotal}
+          increment={this.incrementP1}
+          decrement={this.decrementP1}
+          isTop={true}
+        />
+        <LifeBox
+          lifetotal={this.state.p2LifeTotal}
+          increment={this.incrementP2}
+          decrement={this.decrementP2}
+          isTop={false}
+        />
       </View>
     );
   }
--- mtg-counter/components/LifeBox.js
+++ mtg-counter/components/LifeBox.js
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
 
-class LifeBox extends Component {
-  render() {
-    return (
-      <View style={styles.lifeContainer}>
-        <Text style={styles.lifeText}>20</Text>
-      </View>
-    );
-  }
-}
+const LifeBox = ({ lifetotal, increment, decrement, isTop }) => {
+  return (
+    <View style={[styles.lifeContainer, isTop ? styles.top : ""]}>
+      <TouchableHighlight style={styles.button} onPress={() => increment()}>
+        <Text style={styles.lifeText}>+</Text>
+      </TouchableHighlight>
+      <Text style={styles.lifeText}>{lifetotal}</Text>
+      <TouchableHighlight style={styles.button} onPress={() => decrement()}>
+        <Text style={styles.lifeText}>-</Text>
+      </TouchableHighlight>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   lifeContainer: {
     flex: 1,
+    flexDirection: "row",
     backgroundColor: "#FF0000",
     alignItems: "center",
     justifyContent: "center",
@@ -21,7 +26,19 @@ const styles = StyleSheet.create({
     borderBottomColor: "black",
     borderBottomWidth: 2
   },
+  top: {
+    transform: [{ rotate: "180deg" }]
+  },
+  button: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.0)",
+    alignItems: "center",
+    justifyContent: "center"
+  },
   lifeText: {
+    flex: 1,
+    textAlign: "center",
     fontSize: 80,
     fontWeight: "700",
     color: "#FFF"
